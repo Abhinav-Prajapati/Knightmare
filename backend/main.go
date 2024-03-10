ca9e20055eee7b9db73b15bbd817721f97cf94c9
Abhinav <abhinavprajapait2001@gmail.com>
2024-03-10 06:10:10 +0530

    Add basic functionality for WebSocket communication and chessboard rendering

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/notnil/chess"
 )
+
+type GameService interface {
+	NewGame() *chess.Game
+	GetGameById(id string) (*chess.Game, bool)
+}
+
+type gameService struct {
+	games map[string]*chess.Game
+}
+
+func (gs *gameService) NewGame() (*chess.Game, string) {
+	game := chess.NewGame()
+	id := strconv.Itoa(rand.Intn(10000)) // todo : use uuids
+	gs.games[id] = game
+	return game, id
+}
+
+func (gs *gameService) GetGameById(game_id string) (*chess.Game, bool) {
+	game, ok := gs.games[game_id]
+	return game, ok
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -16,39 +42,86 @@ var upgrader = websocket.Upgrader{
 
 func main() {
 
-	// Configure WebSocket route
-	http.HandleFunc("/ws", handleConnections)
+	games := make(map[string]*chess.Game)
+	gameService := &gameService{games: games}
 
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Allow all origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 3600, // Maximum age of a preflight request
+	}))
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, quick chess!")
+	})
+	router.GET("/newgame", func(c *gin.Context) {
+		_, uuid := gameService.NewGame()
+		c.JSON(http.StatusOK, gin.H{"game_uuid": uuid})
+
+	})
+	router.GET("/game/:gameuuid", func(c *gin.Context) {
+		game_uuid := c.Param("gameuuid")
+		handleConnections(c.Writer, c.Request, gameService, game_uuid)
+	})
 	// Start the server on localhost port 8080
-	log.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := router.Run(":8080")
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("Error starting server: ", err)
 	}
 }
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a WebSocket
+func sendGameState(ws *websocket.Conn, game *chess.Game) {
+	// Get the current FEN representation of the game
+	fen := game.Position().String()
+	fmt.Println("Game : ", fen)
+	// Send the FEN representation to the client
+	err := ws.WriteMessage(websocket.TextMessage, []byte(fen))
+	if err != nil {
+		log.Printf("error writing message to socket connection: %v", err)
+	}
+}
+
+func sendErrorMessage(ws *websocket.Conn, message string) {
+	// Send the error message to the client
+	err := ws.WriteMessage(websocket.TextMessage, []byte(message))
+	if err != nil {
+		log.Printf("error writing message to socket connection: %v", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request, gs *gameService, game_uuid string) {
+	// Upgrade from http to a WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ws.Close()
 
+	game, ok := gs.GetGameById(game_uuid)
+	if !ok {
+		sendErrorMessage(ws, "Game not found")
+		return
+	}
+
 	for {
-		// Read in a new message
-		messageType, msg, err := ws.ReadMessage()
+		_, msg, err := ws.ReadMessage()
+
 		if err != nil {
 			log.Printf("error: %v", err)
 			break
 		}
-		fmt.Println("Message received from client:", string(msg))
+		fmt.Println("Message received from client: ", string(msg))
 
-		// Echo the message back to the client
-		err = ws.WriteMessage(messageType, msg)
+		err = game.MoveStr(string(msg)) // directly moves in game
 		if err != nil {
-			log.Printf("error writing message to socket connection: %v", err)
-			break
+			sendErrorMessage(ws, "Invalid move")
+		} else {
+			sendGameState(ws, game)
 		}
 	}
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,69 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Chessboard } from "react-chessboard";
 
 const WebSocketComponent: React.FC = () => {
   const [msgToSend, setmsgToSend] = useState("");
   const [socketData, setSocketData] = useState<string>("");
   const [conn, setConn] = useState<WebSocket | null>(null);
+  const [uuid, setUuid] = useState("No game");
+  const [gameFen, setGameFen] = useState();
 
-  useEffect(() => {
-    const socket = new WebSocket("ws://localhost:8080/ws"); // Replace with your WebSocket server URL
-    setConn(socket);
+  const newGame = async () => {
+    try {
+      let response = await axios.get("http://localhost:8080/newgame");
 
-    socket.onopen = () => {
-      console.log("WebSocket connected");
-      socket.send("1");
-    };
+      if (response.status === 200) {
+        console.log("New game created successfully");
+        setUuid(response.data["game_uuid"]);
+        console.log("uuid " + response.data["game_uuid"]);
 
-    socket.onmessage = (event) => {
-      setSocketData(event.data);
-    };
+        const socket = new WebSocket(
+          `ws://localhost:8080/game/${response.data["game_uuid"]}`
+        ); // Replace with your WebSocket server URL
+        setConn(socket);
+        socket.onopen = () => {
+          console.log("WebSocket connected");
+        };
+        socket.onmessage = (event) => {
+          setSocketData(event.data);
+          setGameFen(event.data);
+          console.log(socketData);
+        };
+        socket.onclose = () => {
+          console.log("WebSocket disconnected");
+        };
+        // handle closing of socket
+      } else {
+        console.error("Failed to create new game:", response.statusText);
+        // Handle error response here
+      }
+    } catch (error) {
+      console.error("Error creating new game:", error);
+      // Handle any other errors here
+    }
+  };
 
-    socket.onclose = () => {
-      console.log("WebSocket disconnected");
-    };
-
-    return () => {
-      socket.close();
-    };
-  }, []);
+  const handlePieceDrop = (from: string, to: string) => {
+    conn?.send(`${from}${to}`);
+    setGameFen(gameFen);
+    return true;
+  };
 
   return (
-    <div>
-      <div className=" flex flex-row gap-2  ">
+    <div className=" flex">
+      <div className="w-[42vw] p-4">
+        <Chessboard
+          id="BasicBoard"
+          onPieceClick={(piece) => console.log(piece)}
+          onSquareClick={(square) => console.log(`Clicked square ${square}`)}
+          onPieceDrop={handlePieceDrop}
+          position={gameFen}
+        />
+      </div>
+
+      <div className=" flex flex-col gap-2  h-full pl-4 ">
         <h1 className="">Enter data to send</h1>
         <input
           type="text"
@@ -47,10 +80,19 @@ const WebSocketComponent: React.FC = () => {
         >
           Send
         </button>
-      </div>
-      <div className=" flex flex-row pt-2">
-        <h1>Data recived from server : </h1>
-        <p className=" border-2 border-black px-3">{socketData}</p>
+        <div className=" flex flex-row pt-2">
+          <h1>Data recived from server : </h1>
+          <p className=" border-2 border-black px-3">{socketData}</p>
+        </div>
+        <div className=" flex items-center gap-2 ">
+          <button
+            onClick={newGame}
+            className="border-2 border-black p-2 text-xl"
+          >
+            new game
+          </button>
+          <span>uuid: {uuid}</span>
+        </div>
       </div>
     </div>
   );
