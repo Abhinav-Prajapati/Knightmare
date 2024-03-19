1b0bb5f4d7ec0c61724dcb2c47a8f4dc4338e3fe
Abhinav <abhinavprajapait2001@gmail.com>
2024-03-19 06:35:31 +0530

    Add ChessBoard component

---
+++ frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Chessboard } from "react-chessboard";
+
+const ChessBoard = ({ gameFen, playerColor, handlePieceDrop }) => {
+  return (
+    <div className=" w-[50%] bg-gradient-to-tr to-[#A348DF] from-[#7143E2] rounded-2xl">
+      <div className=" p-6 my-5 mx-8  rounded-2xl bg-[#D9D9D933] backdrop-blur-md innter ">
+        <Chessboard
+          id="BasicBoard"
+          onPieceDrop={handlePieceDrop}
+          position={gameFen}
+          boardOrientation={playerColor.toLowerCase()}
+          customDarkSquareStyle={{ backgroundColor: "#D9D9D933" }}
+          customLightSquareStyle={{ backgroundColor: "#ffffffb3" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChessBoard;
