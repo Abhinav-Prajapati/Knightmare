b398704f42279254abe4d623999324a61eb3850f
Abhinav <abhinavprajapati@gmail.com>
2024-03-27 13:58:40 -0400

    Add database connection and dependencies

--- server/db/db.go
+++ server/db/db.go
@@ -1,1 +1,20 @@
 package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	db *gorm.DB
+}
+
+func NewDataBase() (*Database, error) {
+
+	dsn := "host=localhost user=admin password=quickchess@admin dbname=test port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return &Database{db: db}, nil
+}
