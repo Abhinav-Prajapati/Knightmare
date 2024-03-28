778275baf26abd12457664e8fced9b9b426e9bdc
Abhinav <abhinavprajapati@gmail.com>
2024-03-28 15:11:29 -0400

    Add login and logout http handler functionality

--- server/internals/user/user_handler.go
+++ server/internals/user/user_handler.go
@@ -28,6 +28,30 @@ func (h *Handler) CreateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
+func (h *Handler) Login(c *gin.Context) {
+	var user LoginUserReq
+	if err := c.ShouldBindJSON(&user); err != err {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	u, err := h.Service.Login(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.SetCookie("jwt", u.accessToken, 3600, "/", "localhost", false, true)
+	res := &LoginUserRes{
+		ID:       u.ID,
+		Username: u.Username,
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func (h *Handler) Logout(c *gin.Context) {
+	c.SetCookie("jwt", "", -1, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{"message": "logout successful"})
+}
+
 func (h *Handler) Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "server running"})
 }
