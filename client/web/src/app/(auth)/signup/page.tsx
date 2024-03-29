6b047afc71af14cd62317481883f3c2326a6b9bb
Abhinav <abhinavprajapati@gmail.com>
2024-03-29 12:26:31 -0400

    Add form validation and error handling

--- client/web/src/app/(auth)/signup/page.tsx
+++ client/web/src/app/(auth)/signup/page.tsx
@@ -4,25 +4,61 @@ import Image from "next/image";
 import * as React from "react";
 import SignUpWithGoogleImage from '../../../../public/icons8-google-48.png'
 import { SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { ZodRawShape, date, z } from "zod"
+import { signup } from "@/services/Signup";
+import { useRouter } from "next/navigation";
 
-interface InputFieldProps {
-  label: string;
-  placeholder: string;
-}
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email" }),
+  username: z.string().min(3, { message: "Username must be at least 3 characters" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+  confirmPassword: z.string()
+}).refine(data => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ['confirmPassword'],
+});
 
-type FormFields = {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+type FormFields = z.infer<typeof schema>
 
-const onSubmit: SubmitHandler<FormFields> = (data) => {
-  console.log(data)
-}
+// type FormFields = {
+//   username: string;
+//   email: string;
+//   password: string;
+//   confirmPassword: string;
+// }
+
 
 const SignupForm: React.FC = () => {
-  const { register, handleSubmit } = useForm<FormFields>();
+  const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: {
+      errors,
+      isSubmitting
+    } }
+    = useForm<FormFields>({ resolver: zodResolver(schema) });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      const response = await signup(data.username, data.email, data.password)
+      // console.log("sign up data", data)
+      // console.log("response", response)
+      if (response.error === "email already exists") {
+        setError("email", { message: "Email alredy in use" })
+      }
+      if (response.ID == 1) {
+        router.push("/")
+        // console.log("User created")
+      }
+
+    } catch (error) {
+      // setError("email", { message: "Email alredy taken" })
+    }
+  }
+
   return (
     <div className="px-10 pt-12 pb-10 mt-5 mb-4 max-w-full rounded-2xl bg-slate-50 w-[490px] max-md:px-5">
       <form
@@ -34,8 +70,17 @@ const SignupForm: React.FC = () => {
           <br />
         </h1>
 
+        {/* Username */}
         <div className=" flex flex-col mt-4  ">
-          <span className=" text-neutral-800  ">Username</span>
+          {
+            errors.username ? (
+
+              <span className=" text-red-500  ">{errors.username.message}</span>
+            ) : (
+
+              <span className=" text-neutral-800  ">Username</span>
+            )
+          }
           <Input
             {...register("username")}
             className="text-md "
@@ -43,41 +88,74 @@ const SignupForm: React.FC = () => {
           <div className=" h-px border border-solid bg-neutral-400 border-neutral-400" />
         </div>
 
+        {/* Email */}
         <div className=" flex flex-col mt-4  ">
-          <span className=" text-neutral-800  ">Email</span>
+
+          {
+            errors.email ? (
+              <span className="text-red-500">{errors.email.message}</span>
+            ) : (
+
+              <span className=" text-neutral-800  ">Email</span>
+            )
+          }
           <Input
 
             {...register("email")}
             className="text-md "
-            type="text" placeholder="Enter your email" />
+            type="email" placeholder="Enter your email" />
           <div className=" h-px border border-solid bg-neutral-400 border-neutral-400" />
         </div>
 
+        {/* Password */}
         <div className=" flex flex-col mt-4  ">
-          <span className=" text-neutral-800  ">Password</span>
+          {
+            errors.password ? (
+              <span className=" text-red-500">{errors.password?.message}</span>
+            ) : (
+              <span className=" text-neutral-800  ">Password</span>
+            )
+          }
           <Input
             {...register("password")}
             className="text-md "
-            type="text" placeholder="Enter a password" />
+            type="password" placeholder="Enter a password" />
           <div className=" h-px border border-solid bg-neutral-400 border-neutral-400" />
+
         </div>
 
+        {/* Confirm Password */}
         <div className=" flex flex-col mt-4  ">
-          <span className=" text-neutral-800  ">Confirm Password</span>
+          {
+            errors.confirmPassword ? (
+              <span className=" text-red-500  ">{errors.confirmPassword.message}</span>
+            ) : (
+              <span className=" text-neutral-800  ">Confirm Password</span>
+            )
+          }
           <Input
             {...register("confirmPassword")}
-            className="text-md "
-            type="text" placeholder="Confirm passwrod" />
+            className="text-md"
+            type="password" placeholder="Confirm password" />
           <div className=" h-px border border-solid bg-neutral-400 border-neutral-400" />
         </div>
 
         <button
           type="submit"
           className="justify-center items-center px-16 py-5 mt-10 text-xl whitespace-nowrap rounded-[100px] text-white text-opacity-80 max-md:px-5 max-md:mt-10 bg-gradient-to-r from-blue-700 to bg-purple-600"
+          disabled={isSubmitting}
         >
-          SIGNUP
+          {
+            isSubmitting ? (
+              <span>Loading...</span>
+            ) : (
+              <span>
+                SIGNUP
+              </span>
+            )
+          }
         </button>
-      </form>
+      </form >
       <Divider />
       <div className=" w-full flex justify-center ">
         <Image
@@ -93,7 +171,7 @@ const SignupForm: React.FC = () => {
           Login
         </a>
       </div>
-    </div>
+    </div >
   );
 };
 
@@ -122,3 +200,5 @@ const MyComponent: React.FC = () => {
 };
 
 export default MyComponent;
+
+// (React Hook form wiht zod) https://www.youtube.com/watch?v=cc_xmawJ8Kg&ab_channel=CosdenSolutions
