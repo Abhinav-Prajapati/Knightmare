c9c287eaf7cd5d28d0daf2a2c76a12d3ce0280cb
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-01-23 10:30:10 +0530

    fix pieces shift issue
    see diff for more info

--- client/web/src/components/ChessBoard.tsx
+++ client/web/src/components/ChessBoard.tsx
@@ -9,20 +9,21 @@ interface ChessBoardProps {
 }
 
 const ChessBoard: React.FC<ChessBoardProps> = ({ gameFen, playerColor, handlePieceDrop }) => {
-  const lightSquareColor = "#ffffffb3"  
+  const lightSquareColor = "#ffffffb3"
   const darkSquareColor = "#D9D9D933"
   return (
-    <div className="bg-gradient-to-tr to-[#A348DF] from-[#7143E2] rounded-2xl xl:w-[47.5vw] h-max  ">
-      <div className=" p-6 my-5 mx-8  rounded-2xl bg-[#D9D9D933] backdrop-blur-md innter ">
+    <div className="bg-gradient-to-tr to-[#A348DF] from-[#7143E2] rounded-2xl h-full p-4">
+      <div className="p-4 rounded-2xl border">
         <Chessboard
           id="BasicBoard"
           onPieceDrop={handlePieceDrop}
           position={gameFen}
           boardOrientation={playerColor.toLowerCase()}
-          customDarkSquareStyle={{ backgroundColor: darkSquareColor  }}
-          customLightSquareStyle={{ backgroundColor:  lightSquareColor  }}
+          customDarkSquareStyle={{ backgroundColor: darkSquareColor }}
+          customLightSquareStyle={{ backgroundColor: lightSquareColor }}
+          boardWidth={800} // TODO: change this dynamicly
         />
-      </div>
+      </div >
     </div>
   );
 };
