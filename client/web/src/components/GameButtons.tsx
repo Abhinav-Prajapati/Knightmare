15faa667f8df7df463378d0840dfe008f7c29eac
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-02-21 21:45:04 +0530

    update game button

--- client/web/src/components/GameButtons.tsx
+++ client/web/src/components/GameButtons.tsx
@@ -3,10 +3,9 @@ import React from 'react'
 
 const GameButtons = () => {
   return (
-    <div className=' w-full flex justify-between gap-x-1 '>
+    <div className='w-full flex justify-between gap-x-2'>
       <button className='border bg-[#36454F4d] text-xl text-white/80 border-white/40 px-10 py-2 w-1/3 rounded-xl flex-1 hover:bg-[#36454f8c] hover:text-white/90' >Draw</button>
       <button className='border bg-[#36454F4d] text-xl text-white/80 border-white/40 px-10 py-2 w-1/3 rounded-xl flex-1'>Resign</button>
-      <button className='border bg-[#36454F4d] text-xl text-white/80 border-white/40 px-10 py-2 w-1/3 rounded-xl  flex-1'>Rollback</button>
     </div>
   )
 }
