de543141dc37ecdcc5e2fa05eaf5638407c0a0c7
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-03-09 03:03:31 +0530

    🔥 remove code for createing chessgame

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,9 @@
 'use client'
-import ChessBoard from "@/components/ChessBoard";
-import SideMenu from "@/components/SideMenu";
-import Navbar from "@/components/Navbar";
-
 
 const WebSocketComponent: React.FC = () => {
   return (
     <>
-      <Navbar />
-      <div className=" flex  justify-between">
-        <div className=" w-full  border">
-          <SideMenu />
-        </div>
-        <div className=" flex  ">
-        </div>
-        <div className="w-full border"></div>
+      <div className=" h-screen flex  justify-between">
       </div>
     </>
   );
