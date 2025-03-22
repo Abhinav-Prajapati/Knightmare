a9569ccb8782a9ce85faa192f578e9e589cae327
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-03-22 18:53:07 +0530

    ✨: rename web to client

--- client/src/app/room/page.tsx
+++ client/src/app/room/page.tsx
@@ -171,7 +171,6 @@ const WebSocketComponent: React.FC = () => {
           <div className="w-1/4 flex flex-col gap-4 h-[calc(100vh-theme(spacing.24))]">
             <div className="flex-1 flex flex-col gap-4 overflow-hidden">
               <div className="h-[50%] overflow-auto rounded-lg ">
-                <MoveHistory />
               </div>
               <div className="h-[50%] overflow-auto rounded-lg ">
                 <Chat />
