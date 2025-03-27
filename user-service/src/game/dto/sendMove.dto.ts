4b5cc5fc991de7b8f2311f8f96e75be20c8f0d6a
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-03-27 20:50:05 +0530

    fix: streamline ChessMoveDto by removing unused properties and ensuring playerId is required

--- user-service/src/game/dto/sendMove.dto.ts
+++ user-service/src/game/dto/sendMove.dto.ts
@@ -1,35 +1,15 @@
-import { Type } from 'class-transformer';
-import {
-  IsString,
-  IsNotEmpty,
-  IsOptional,
-  IsDate,
-  IsNumber,
-} from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class ChessMoveDto {
-  @IsString()
-  @IsNotEmpty({ message: 'Player ID is required' })
-  playerId: string;
-
   @IsString()
   @IsNotEmpty({ message: 'Game ID is required' })
   gameId: string;
 
   @IsString()
-  @IsOptional()
-  UCImove?: string;
+  @IsNotEmpty({ message: 'Player ID is required' })
+  playerId: string;
 
   @IsString()
   @IsOptional()
-  SANmove?: string;
-
-  @IsOptional()
-  @IsDate()
-  @Type(() => Date)
-  timestamp?: Date;
-
-  @IsOptional()
-  @IsNumber()
-  difficulty?: number; // recive the difficulty level of engine to return move for needed for computer game and should be ignored for multiplayer games
+  UCImove?: string;
 }
