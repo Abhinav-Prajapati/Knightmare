1b057a74d222c09f6eeefb38b590c57b64a2fdea
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-03-28 20:39:36 +0530

    refactor: remove ChatGateway and related WebSocket functionality from game module

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -6,7 +6,6 @@ import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { GameModule } from './game/game.module';
-import { ChatGateway } from './game/game.gateway';
 import { AuthGuard } from './user/auth.guard';
 import { ChessEngineGateway } from './game/game.engine.gateway';
 
@@ -23,7 +22,6 @@ import { ChessEngineGateway } from './game/game.engine.gateway';
   providers: [
     AppService,
     UserService,
-    ChatGateway,
     GameModule,
     AuthGuard,
     ChessEngineGateway,
